86101cbb7064e51e2fdf3df5500e72ac67d3b03f
sheelshah9 <shahsheel667@gmail.com>
2020-07-15 01:12:01 -0400

    added LSTM support

--- predict.py
+++ predict.py
@@ -9,25 +9,25 @@ grouped_df, daily_df = preprocess_data(df)
 pred_path = "data/"
 
 reg = Regressor()
-# forecasted_days_arima, real_arima, intervals_arima = reg.ARIMA(daily_df, interval_forecast=10)
-# with open(pred_path+'forecast_daily_cases_arima', 'wb') as fp:
-#     pickle.dump([forecasted_days_arima, real_arima, intervals_arima], fp)
-#
-# forecasted_days_xg, real_xg, intervals_xg = reg.XGBoost(daily_df, interval_forecast=10)
-# with open(pred_path+'forecast_daily_cases_xgboost', 'wb') as fp:
-#     pickle.dump([forecasted_days_xg, real_xg, intervals_xg], fp)
+forecasted_days_arima, real_arima, intervals_arima = reg.ARIMA(daily_df, interval_forecast=10)
+with open(pred_path+'forecast_daily_cases_arima', 'wb') as fp:
+    pickle.dump([forecasted_days_arima, real_arima, intervals_arima], fp)
+
+forecasted_days_xg, real_xg, intervals_xg = reg.XGBoost(daily_df, interval_forecast=10)
+with open(pred_path+'forecast_daily_cases_xgboost', 'wb') as fp:
+    pickle.dump([forecasted_days_xg, real_xg, intervals_xg], fp)
 
 forecasted_days_lstm, real_lstm, intervals_lstm = reg.LSTM(daily_df, interval_forecast=7)
 with open(pred_path+'forecast_daily_cases_lstm', 'wb') as fp:
     pickle.dump([forecasted_days_lstm, real_lstm, intervals_lstm], fp)
 
-# forecasted_days_arima, real_arima, intervals_arima = reg.ARIMA(grouped_df, interval_forecast=10)
-# with open(pred_path+'forecast_total_cases_arima', 'wb') as fp:
-#     pickle.dump([forecasted_days_arima, real_arima, intervals_arima], fp)
-#
-# forecasted_days_xg, real_xg, intervals_xg = reg.XGBoost(grouped_df, interval_forecast=10)
-# with open(pred_path+'forecast_total_cases_xgboost', 'wb') as fp:
-#     pickle.dump([forecasted_days_xg, real_xg, intervals_xg], fp)
+forecasted_days_arima, real_arima, intervals_arima = reg.ARIMA(grouped_df, interval_forecast=10)
+with open(pred_path+'forecast_total_cases_arima', 'wb') as fp:
+    pickle.dump([forecasted_days_arima, real_arima, intervals_arima], fp)
+
+forecasted_days_xg, real_xg, intervals_xg = reg.XGBoost(grouped_df, interval_forecast=10)
+with open(pred_path+'forecast_total_cases_xgboost', 'wb') as fp:
+    pickle.dump([forecasted_days_xg, real_xg, intervals_xg], fp)
 
 forecasted_days_lstm, real_lstm, intervals_lstm = reg.LSTM(grouped_df, interval_forecast=7)
 with open(pred_path+'forecast_total_cases_lstm', 'wb') as fp:
